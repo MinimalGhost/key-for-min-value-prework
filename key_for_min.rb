021a74e631fdc35aade72513e3c15a233511e000
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-10 23:53:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   default_key = 0
 
   name_a.each do |i|
-    if i[1] < d_value
+    if i[1] < default_value
     default_value = i[1]
     default_key = i[0]
   end
