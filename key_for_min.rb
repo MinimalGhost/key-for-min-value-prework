2173aa0a4cf8b497f09133c72352f6d3a30bcdb3
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-11 13:58:15 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,14 @@
 
 def key_for_min_value(name_hash)
   # Convert hash to array
-   name_a = name_hash.to_a
+  #  name_a = name_hash.to_a
 
    # Default key value
    d_value= 1000
    d_key= 0
 
    # Iterate new array
-   name_a.each do |i|
+   name_hash.each do |i|
        # If current value is lower than default, change value&key
        if i[0][0] < d_value
          d_value = i[1]
