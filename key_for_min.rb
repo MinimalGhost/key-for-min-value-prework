951f1a07baaa67b7770d803f4935ca1c26f08e4c
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-11 13:42:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |k, v|
     if v <= lowest_value
       lowest_value = v
+    else
+      return nil
+
     end
   end
-  return lowest_value
 end
